1430517352ba3b498a5f7dd1299a35642550246b
J A I M I N R A V A T <jemmyravat@gmail.com>
2026-01-19 23:15:20 +0530

    Refactor user routes to use User model

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,14 +1,24 @@
 import express from "express";
-import {
-  getUsers,
-  addUser,
-  deleteUser
-} from "../controllers/userController.js";
+import User from "../models/User.js";
 
 const router = express.Router();
 
-router.get("/", getUsers);
-router.post("/", addUser);
-router.delete("/:id", deleteUser);
+// GET users
+router.get("/", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+
+// ADD user
+router.post("/", async (req, res) => {
+  const user = await User.create(req.body);
+  res.json(user);
+});
+
+// DELETE user
+router.delete("/:id", async (req, res) => {
+  await User.findByIdAndDelete(req.params.id);
+  res.json({ message: "User deleted" });
+});
 
 export default router;
