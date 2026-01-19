950868cc2b6fa6d5973e13f0548726ef07ccc633
J A I M I N R A V A T <jemmyravat@gmail.com>
2026-01-19 23:51:28 +0530

    Refactor server.js to use userRoutes and serve frontend

--- Backend/server.js
+++ Backend/server.js
@@ -1,30 +1,26 @@
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
-import User from "./models/User.js";
+import userRoutes from "./routes/userRoutes.js";
+import path from "path";
 
 const app = express();
 connectDB();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public")); // 👈 HTML serve karega
 
-// FORM SUBMIT
-app.post("/add-user", async (req, res) => {
-  const { name, email } = req.body;
-  await User.create({ name, email });
-  res.redirect("/"); // wapas page par
-});
+// 👇 frontend serve
+app.use(express.static("public"));
 
-// DATA FETCH
-app.get("/users", async (req, res) => {
-  const users = await User.find();
-  res.json(users);
+app.use("/api/users", userRoutes);
+
+// fallback
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("public/index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>
-  console.log("Server running 🚀")
-);
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
