4560a8e5fa9a7af807af0d2ba7eee86d7e4e449e
J A I M I N R A V A T <jemmyravat@gmail.com>
2026-01-20 09:13:18 +0530

    Update auth.js

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,11 +1,11 @@
 import jwt from "jsonwebtoken";
 
 export default function auth(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ msg: "No token, auth denied" });
-  }
-  const token = authHeader.split(" ")[1];
+  const header = req.headers.authorization;
+  if (!header) return res.status(401).json({ msg: "No token" });
+
+  const token = header.split(" ")[1];
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
