e918d212f76fa5ea5a48bfbbf70aa9db6dddc538
J A I M I N R A V A T <jemmyravat@gmail.com>
2026-01-20 19:25:04 +0530

    Update todoRoutes.js

--- Backend/routes/todoRoutes.js
+++ Backend/routes/todoRoutes.js
@@ -1,17 +1,40 @@
 import express from "express";
 import auth from "../middleware/auth.js";
-import {
-  getTodos,
-  addTodo,
-  toggleTodo,
-  deleteTodo
-} from "../controllers/todoController.js";
+import Todo from "../models/Todo.js";
 
 const router = express.Router();
 
-router.get("/", auth, getTodos);
-router.post("/", auth, addTodo);
-router.patch("/:id", auth, toggleTodo);
-router.delete("/:id", auth, deleteTodo);
+// GET todos
+router.get("/", auth, async (req, res) => {
+  const todos = await Todo.find({ user: req.user.id }).sort({ createdAt: -1 });
+  res.json(todos);
+});
+
+// ADD todo
+router.post("/", auth, async (req, res) => {
+  const todo = await Todo.create({
+    ...req.body,
+    done: false,
+    user: req.user.id
+  });
+  res.json(todo);
+});
+
+// TOGGLE done
+router.patch("/:id", auth, async (req, res) => {
+  const todo = await Todo.findOne({
+    _id: req.params.id,
+    user: req.user.id
+  });
+  todo.done = !todo.done;
+  await todo.save();
+  res.json(todo);
+});
+
+// DELETE
+router.delete("/:id", auth, async (req, res) => {
+  await Todo.deleteOne({ _id: req.params.id, user: req.user.id });
+  res.json({ success: true });
+});
 
 export default router;
