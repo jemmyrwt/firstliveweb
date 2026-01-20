0035981f307a5c56c81457ccb658ca7dc997857b
J A I M I N R A V A T <jemmyravat@gmail.com>
2026-01-20 18:17:05 +0530

    Update todoRoutes.js

--- Backend/routes/todoRoutes.js
+++ Backend/routes/todoRoutes.js
@@ -1,25 +1,17 @@
 import express from "express";
 import auth from "../middleware/auth.js";
-import Todo from "../models/Todo.js";
+import {
+  getTodos,
+  addTodo,
+  toggleTodo,
+  deleteTodo
+} from "../controllers/todoController.js";
 
 const router = express.Router();
 
-router.get("/", auth, async (req, res) => {
-  const todos = await Todo.find({ user: req.user.id });
-  res.json(todos);
-});
-
-router.post("/", auth, async (req, res) => {
-  const todo = await Todo.create({
-    text: req.body.text,
-    user: req.user.id
-  });
-  res.json(todo);
-});
-
-router.delete("/:id", auth, async (req, res) => {
-  await Todo.deleteOne({ _id: req.params.id, user: req.user.id });
-  res.json({ success: true });
-});
+router.get("/", auth, getTodos);
+router.post("/", auth, addTodo);
+router.patch("/:id", auth, toggleTodo);
+router.delete("/:id", auth, deleteTodo);
 
 export default router;
