72d5870937b5f40538312edb9a8b2355980aa4c9
J A I M I N R A V A T <jemmyravat@gmail.com>
2026-01-20 06:38:22 +0530

    Refactor server.js to improve structure and clarity

--- Backend/server.js
+++ Backend/server.js
@@ -1,25 +1,35 @@
 import express from "express";
 import cors from "cors";
-import connectDB from "./config/db.js";
-import userRoutes from "./routes/userRoutes.js";
 import path from "path";
+import connectDB from "./config/db.js";
+
+// routes
+import userRoutes from "./routes/userRoutes.js";
+import todoRoutes from "./routes/todoRoutes.js";
 
 const app = express();
+
+// DB connect
 connectDB();
 
+// middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// 👇 frontend serve
+// frontend (HTML / React build / public)
 app.use(express.static("public"));
 
+// APIs
 app.use("/api/users", userRoutes);
+app.use("/api/todos", todoRoutes);
 
-// fallback
+// fallback (important for refresh)
 app.get("*", (req, res) => {
   res.sendFile(path.resolve("public/index.html"));
 });
 
+// server start
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
