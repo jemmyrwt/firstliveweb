724b33bd64db77a35236dc1fe50dc75f79a873fc
J A I M I N R A V A T <jemmyravat@gmail.com>
2026-01-20 09:35:09 +0530

    Update User.js

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
 
-export default mongoose.model(
-  "User",
-  new mongoose.Schema({
-    name: String,
-    email: String
-  })
-);
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: String
+});
+
+export default mongoose.model("User", userSchema);
