81e6628f710fd262efeed791c9d4a07e75647a15
J A I M I N R A V A T <jemmyravat@gmail.com>
2026-01-20 18:15:32 +0530

    Update Todo.js

--- Backend/models/Todo.js
+++ Backend/models/Todo.js
@@ -1,15 +1,21 @@
 import mongoose from "mongoose";
 
-const todoSchema = new mongoose.Schema({
-  text: {
-    type: String,
-    required: true
+const todoSchema = new mongoose.Schema(
+  {
+    title: String,
+    prio: String,
+    cat: String,
+    date: String,
+    done: Boolean,
+
+    // 🔑 kis user ka todo
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    }
   },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true
-  }
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("Todo", todoSchema);
